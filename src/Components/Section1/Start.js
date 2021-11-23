9dd4e834d8b43f18d24e310765ee0e35cd9d7b46
Viggo <viggo.lagerstedtekholm@gmail.com>
2021-11-23 03:14:34 +0100

    Initial commit.

---
+++ src/Components/Section1/Start.js
@@ -0,0 +1,19 @@
+function Start() {
+    return (
+        <section id="#sec-1">
+            <div className="row">
+                <div className="typing-demo">
+                    PayBuddy, your app for logging owed money.
+                </div>
+            </div>
+
+            <div className="row">
+                <a href="#sec-2">
+                    <div className="scroll-down"/>
+                </a>
+            </div>
+        </section>
+    );
+}
+
+export default Start;
